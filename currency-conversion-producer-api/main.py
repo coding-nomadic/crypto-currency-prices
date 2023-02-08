b9f77e5f8f9b93f958ad1bac73d88edfb85d5ab7
Tenzin Dawa <tnzngdw@gmail.com>
2023-02-09 00:47:15 +0530

    Update main.py

--- currency-conversion-producer-api/main.py
+++ currency-conversion-producer-api/main.py
@@ -1,8 +1,6 @@
 import yaml
 import logging
 from flask import Flask, jsonify
-from apscheduler.schedulers.background import BackgroundScheduler
-from kafka import KafkaProducer
 import requests
 import json
 import time
@@ -21,10 +19,6 @@ with open('config.yml', 'r') as stream:
     config = yaml.safe_load(stream)
 app.config.update(config)
 
-# configuration for kafka 
-producer = KafkaProducer(bootstrap_servers=[app.config['kafka_server_url']])
-scheduler = BackgroundScheduler()
-
 error_response = {"status": "Internal Server Error"}
 
 
@@ -32,7 +26,7 @@ error_response = {"status": "Internal Server Error"}
 @app.route('/from/<one_code>/to/<another_code>/with/<amount>')
 def convert_currency(one_code, another_code, amount):
     endpoint_url = app.config['convert_url'] % (another_code, one_code, amount)
-    logger.info(f'Incoming request : ' + endpoint_url)
+    logger.info("Incoming request : ".format(endpoint_url))
     try:
         response = requests.request('GET', endpoint_url, headers={'apikey': app.config['api_key']}, data={})
         response.raise_for_status()
@@ -46,7 +40,7 @@ def convert_currency(one_code, another_code, amount):
 @app.route('/fluctuation/<start_date>/<end_date>')
 def fluctuate_currency(start_date, end_date):
     endpoint_url = app.config['fluctuate_url'] % (start_date, end_date)
-    logger.info(f'Incoming request : ' + endpoint_url)
+    logger.info("Incoming request : ".format(endpoint_url))
     try:
         response = requests.request('GET', endpoint_url, headers={'apikey': app.config['api_key']}, data={})
         response.raise_for_status()
@@ -56,21 +50,6 @@ def fluctuate_currency(start_date, end_date):
     return response.json(), 200
 
 
-# send message to Kafka Topic
-def publish_message():
-    try:
-        topic_name = app.config['kafka_topic']
-        endpoint_url = app.config['topic_api_url']
-        response = requests.request('GET', endpoint_url, headers={'apikey': app.config['api_key']}, data={})
-        producer.send(topic_name, json.dumps(response.json()).encode('utf-8'))
-        time.sleep(10)
-        logger.info('Message successfully sent to Kafka Topic : ' + topic_name)
-    except Exception as e:
-        logger.error("Error occurred {}".format(e))
-
-
 # main class.
 if __name__ == '__main__':
-    scheduler.add_job(func=publish_message, trigger="interval", seconds=20)
-    scheduler.start()
-    app.run(debug=True, port=8080)
+    app.run(debug=False)
