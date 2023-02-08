0f4d212d3459a2c7cba0eed9fae9f109797be0f0
Tenzin Dawa <tnzngdw@gmail.com>
2023-02-08 21:37:50 +0530

    Update main.py

--- currency-conversion-producer-api/main.py
+++ currency-conversion-producer-api/main.py
@@ -25,20 +25,22 @@ app.config.update(config)
 producer = KafkaProducer(bootstrap_servers=[app.config['kafka_server_url']])
 scheduler = BackgroundScheduler()
 
-error_response={"status":"Internal Server Error"}
+error_response = {"status": "Internal Server Error"}
 
-# convert curreny from one currency to another currency code with amout.
+
+# convert currency from one currency to another currency code with amount.
 @app.route('/from/<one_code>/to/<another_code>/with/<amount>')
 def convert_currency(one_code, another_code, amount):
     endpoint_url = app.config['convert_url'] % (another_code, one_code, amount)
     logger.info(f'Incoming request : ' + endpoint_url)
     try:
-       response = requests.request('GET', endpoint_url, headers={'apikey': app.config['api_key']}, data={}) 
-       response.raise_for_status()
+        response = requests.request('GET', endpoint_url, headers={'apikey': app.config['api_key']}, data={})
+        response.raise_for_status()
     except requests.exceptions.RequestException as e:
-       logger.error(e) 
-       return error_response, 500
-    return response.json(),200
+        logger.error("Error occurred : {}".format(e))
+        return error_response, 500
+    return response.json(), 200
+
 
 # defines fluctuation in currency from certain period.
 @app.route('/fluctuation/<start_date>/<end_date>')
@@ -46,26 +48,29 @@ def fluctuate_currency(start_date, end_date):
     endpoint_url = app.config['fluctuate_url'] % (start_date, end_date)
     logger.info(f'Incoming request : ' + endpoint_url)
     try:
-       response = requests.request('GET', endpoint_url, headers={'apikey': app.config['api_key']}, data={}) 
-       response.raise_for_status()
+        response = requests.request('GET', endpoint_url, headers={'apikey': app.config['api_key']}, data={})
+        response.raise_for_status()
     except requests.exceptions.RequestException as e:
-       logger.error(e) 
-       return error_response, 500
-    return response.json(),200
+        logger.error("Error occurred : {}".format(e))
+        return error_response, 500
+    return response.json(), 200
 
 
 # send message to Kafka Topic
 def publish_message():
-    topic_name = app.config['kafka_topic']
-    endpoint_url = app.config['topic_api_url']
-    response = requests.request('GET', endpoint_url, headers={'apikey': app.config['api_key']}, data={})
-    producer.send(topic_name, json.dumps(response.json()).encode('utf-8'))
-    time.sleep(10)
-    logger.info('Message successfully sent to Kafka Topic : ' + topic_name)
+    try:
+        topic_name = app.config['kafka_topic']
+        endpoint_url = app.config['topic_api_url']
+        response = requests.request('GET', endpoint_url, headers={'apikey': app.config['api_key']}, data={})
+        producer.send(topic_name, json.dumps(response.json()).encode('utf-8'))
+        time.sleep(10)
+        logger.info('Message successfully sent to Kafka Topic : ' + topic_name)
+    except Exception as e:
+        logger.error("Error occurred {}".format(e))
 
 
 # main class.
 if __name__ == '__main__':
     scheduler.add_job(func=publish_message, trigger="interval", seconds=20)
     scheduler.start()
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
